d83d6ff18457e45787d9246bf3b2fccc93465a3a
Omar <omaroobaniessa@gmail.com>
2020-06-03 04:21:22 -0400

    Modified and faster version

--- httpalive.cpp
+++ httpalive.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <thread>
+#include <omp.h>
 
+
+#define NUM_THREADS 40
 
 using namespace std;
 
@@ -12,43 +14,72 @@ size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp)
    return size * nmemb;
 }
 
-void Test(CURL *c, string url, bool Flag);
-
 
 int main(int argc, char const *argv[] )
 {
-  CURL *curl = curl_easy_init();
-
-  ifstream http(argv[1]);
-  string line;
   
-  while (getline(http, line)){
-      thread t1(Test,curl, line, true);
-      thread t2(Test, curl, line, false);
-      t1.join();
-      t2.join();
-
-  }
 
 
-  curl_easy_cleanup(curl);
-  return 0;
+	if (argc < 2) cout << "Usage httplive <path to subdomains>" << endl;
+
+	ifstream infile(argv[1]);
+	string line;
+	omp_set_num_threads(NUM_THREADS/2);
+
+	#pragma omp parallel
+	{
+
+
+		for (string line ; getline(infile, line); ) {
+
+			CURL *curl = curl_easy_init();
+			line = "http://" + line ;
+			curl_easy_setopt(curl, CURLOPT_URL, const_cast<char*>(line.c_str()));
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+			curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 0L); 
+			curl_easy_setopt(curl, CURLOPT_TCP_NODELAY, 0L);
+			curl_easy_setopt(curl, CURLOPT_TIMEOUT, 2L);     
+			curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "");
+			curl_easy_setopt(curl, CURLOPT_COOKIE, "");
+			curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+			curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+			curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
+			auto res = curl_easy_perform(curl);
+
+
+		if (res == CURLE_OK ) cout << line << endl;
+
+		}
+	}
+
+	omp_set_num_threads(NUM_THREADS/2);
+
+	ifstream infile2(argv[1]);
+	#pragma omp parallel
+	{
+	
+
+		for (string line ; getline(infile2, line); ) {
+
+			CURL *curl = curl_easy_init();
+			line = "https://" + line ;
+			curl_easy_setopt(curl, CURLOPT_URL, const_cast<char*>(line.c_str()));
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+			curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 0L); 
+			curl_easy_setopt(curl, CURLOPT_TCP_NODELAY, 0L);
+			curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);     
+			curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "");
+			curl_easy_setopt(curl, CURLOPT_COOKIE, "");
+			curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+			curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+			curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
+			auto res = curl_easy_perform(curl);
+		
+		if (res == CURLE_OK ) cout << line << endl;
+
+		}
+	}
+
+	cout << "Finished proccessing " << endl;
+	return 0;
 }
-
-void Test(CURL *curl, string line,  bool Flag){
-    
-    
-    CURLcode res;
-    if (curl) {
-
-      line = Flag ?  "https://" + line : "http://"+ line;
-      curl_easy_setopt(curl, CURLOPT_URL, const_cast<char*>(line.c_str()));
-      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-      res = curl_easy_perform(curl);
-      if (res == CURLE_OK || res == 2) cout << line << endl;
-
-  }
-
-}
-
-
